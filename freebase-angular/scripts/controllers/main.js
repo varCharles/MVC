252aca729a5c9ba635f84cdc9fc0385b9f946066
Anthony Charles <acharles@iomedia.com>
2013-08-12 21:20:41 -0700

    added API Key

--- freebase-angular/scripts/controllers/main.js
+++ freebase-angular/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 angular.module('googleApp')
   	.controller('MoviesCtrl', function ($scope, $http) {
    		$scope.getQuery = function(){
-	    	$http.jsonp('https://www.googleapis.com/freebase/v1/search?query=' + $scope.freebaseQuery +'&filter=(all+type%3A%2Ffilm%2Ffilm)&callback=JSON_CALLBACK').success(
+	    	$http.jsonp('https://www.googleapis.com/freebase/v1/search?query=' + $scope.freebaseQuery +'&filter=(all+type%3A%2Ffilm%2Ffilm)&callback=JSON_CALLBACK&key=AIzaSyC8nRgXdpK16t5aiXpXexUVEIWma1RBNlU').success(
 			function( payload ){
 				$scope.movies = payload.result;
 			}
@@ -12,7 +12,7 @@ angular.module('googleApp')
   })
   .controller('MovieCtrl', function ($scope, $http, $routeParams) {
   		$scope.movieid = $routeParams.movieId
-    	$http.jsonp('https://www.googleapis.com/freebase/v1/topic/m/'+ $scope.movieid +'?callback=JSON_CALLBACK').success(
+    	$http.jsonp('https://www.googleapis.com/freebase/v1/topic/m/'+ $scope.movieid +'?callback=JSON_CALLBACK&key=AIzaSyC8nRgXdpK16t5aiXpXexUVEIWma1RBNlU').success(
 			function( payload ){
 				$scope.movie = payload.property;
 				$scope.actors = $scope.movie['/film/film/starring'].values;
@@ -23,7 +23,7 @@ angular.module('googleApp')
   })
 	.controller('ActorCtrl', function ($scope, $http, $routeParams) {
   		$scope.actorid = $routeParams.actorId
-    	$http.jsonp('https://www.googleapis.com/freebase/v1/topic/m/'+ $scope.actorid +'?callback=JSON_CALLBACK').success(
+    	$http.jsonp('https://www.googleapis.com/freebase/v1/topic/m/'+ $scope.actorid +'?callback=JSON_CALLBACK&key=AIzaSyC8nRgXdpK16t5aiXpXexUVEIWma1RBNlU').success(
 			function( payload ){
 				$scope.actor = payload.property;
 				$scope.movies = $scope.actor['/film/actor/film'].values;
